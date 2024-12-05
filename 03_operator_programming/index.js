9bf462c75021697c7d16455aadbd387c2d75c4e2
Rathod Yogesh <yogeshrd1708@gmail.com>
2024-12-05 15:12:16 +0530

    Add files via upload
    operator_program  05/12/24

---
+++ 03_operator_programming/index.js
@@ -0,0 +1,109 @@
+
+//  < ----  opretoes programing ------>
+
+//  1. convert to doller in to rupee
+console.log("1. convert to doller in to rupee")
+
+let doller = 37, rupee
+
+rupee = doller * 84.71;
+console.log("dollar" , doller)
+console.log("rupee" ,rupee )
+
+
+// 2. convert feet to inches
+console.log("2. convert feet to inches")
+
+let feet = 7, inches
+inches = feet * 12;
+console.log("feet" , feet)
+console.log("inches" ,inches )
+
+
+// 3. convert meter to inches
+console.log("3. convert meter to inches")
+
+let meter = 45, inche
+inche = meter * 39;
+console.log("meter" , meter)
+console.log("inches" ,inches )
+
+
+// 4. convert celsius in  to fahrenheit
+console.log("4. convert celsius in  to fahrenheit")
+let celsius = 22, fahrenheit
+
+fahrenheit = (celsius * 9/5) + 32;
+console.log("celsius" , celsius)
+console.log("fahrenheit" ,fahrenheit )
+
+
+// 5. find out area of circle
+console.log("5. find out area of circle")
+let r =12 , area
+
+area = 3.14 * r * r;
+console.log("r",r)
+console.log("area", area)
+
+
+// 6. find out area of traigles 
+{
+    console.log("6. find out area to triangle")
+    let  base = 15 , height = 10, area
+    
+    area = 1/2 * base * height
+    console.log("base" , base ,"height", height)
+    console.log("area", area) 
+}
+
+// 7. find out area of rectangle
+{
+    console.log("7. find out area of rectangle")
+    let  lenght = 21 , width = 10, area
+    
+    area =  lenght * width
+    console.log("lenght" , lenght , "width", width )
+    console.log("area", area) 
+}
+
+// 8. find out simple interest
+  console.log("8. find out simple interest")
+ let principal = 12000,  rate = 8, time = 5
+
+ console.log("principal", principal)
+ console.log("rate", rate)
+ console.log("time", time)
+
+ interest = (principal * rate * time)/100;
+ console.log("interest", interest)
+
+
+//  9. find out three sub total percentage
+console.log("9. find out three sub total percentage")
+let chemistry = 65, biology =75, physics = 56, percentage
+
+sub = chemistry + biology + physics
+console.log("chemistry", chemistry)
+ console.log("biology", biology)
+ console.log("physics", physics)
+
+percentage = (sub/300)*100;
+console.log("percentage", percentage)
+ 
+
+// 10  employee salary to caculate HRA and DA  
+console.log("10. employee salary to caculate HRA and DA")
+
+let Basicsalary =  15000, hra, da
+
+hra = (Basicsalary * 0.15)
+da  =  (Basicsalary * 0.08)
+
+console.log("Basicsalary", Basicsalary)
+console.log("HRA", hra)
+console.log("DA", da)
+
+saleryAmt = Basicsalary + hra + da;
+console.log("saleryAmt", saleryAmt)
+
